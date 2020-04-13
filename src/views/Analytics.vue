a70d306a4d9aa53abe746033d363854ca2ad2503
Sergio García Prado <sergio@garciparedes.me>
2020-04-13 14:58:17 +0200

    ISSUE #? * Minor changes

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,28 +1,28 @@
 <template>
-    <v-container justify="center" dense fluid>
-        <v-row dense align="stretch">
-            <v-col md="8" sm="12">
-                <LineChartExample></LineChartExample>
-            </v-col>
-            <v-col md="4" sm="12">
-                <BarChartExample></BarChartExample>
-            </v-col>
-        </v-row>
-        <v-row dense align="stretch">
-            <v-col md="3" sm="12">
-                <DoughnutChartRequest></DoughnutChartRequest>
-            </v-col>
-            <v-col md="3" sm="12">
-                <DoughnutChartSupply></DoughnutChartSupply>
-            </v-col>
-            <v-col md="3" sm="12">
-                <PercentajeExampleRed></PercentajeExampleRed>
-            </v-col>
-            <v-col md="3" sm="12">
-                <PercentajeExample></PercentajeExample>
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container justify="center" dense fluid id="statistics">
+    <v-row dense align="stretch">
+      <v-col md="8" sm="12">
+        <LineChartExample></LineChartExample>
+      </v-col>
+      <v-col md="4" sm="12">
+        <BarChartExample></BarChartExample>
+      </v-col>
+    </v-row>
+    <v-row dense align="stretch">
+      <v-col md="3" sm="12">
+        <DoughnutChartRequest></DoughnutChartRequest>
+      </v-col>
+      <v-col md="3" sm="12">
+        <DoughnutChartSupply></DoughnutChartSupply>
+      </v-col>
+      <v-col md="3" sm="12">
+        <PercentajeExampleRed></PercentajeExampleRed>
+      </v-col>
+      <v-col md="3" sm="12">
+        <PercentajeExample></PercentajeExample>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -35,11 +35,31 @@
 
     export default {
         name: "Statistics",
-        components: {PercentajeExample, BarChartExample, PercentajeExampleRed, LineChartExample, DoughnutChartSupply, DoughnutChartRequest},
+        components: {
+            PercentajeExample,
+            BarChartExample,
+            PercentajeExampleRed,
+            LineChartExample,
+            DoughnutChartSupply,
+            DoughnutChartRequest
+        },
         methods: {
             addSupply: function () {
                 this.$router.push('/supplies/new');
             },
+        },
+        mounted() {
+            const el = document.getElementById("statistics");
+            if (el) {
+                let headerOffset = 48;
+                let elementPosition = el.getBoundingClientRect().top;
+                let offsetPosition = elementPosition - headerOffset + window.scrollY;
+
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: "smooth"
+                });
+            }
         }
     }
 </script>
